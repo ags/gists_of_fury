e0948dd3de6ac393fb12504902c18907473aa0ac
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-10-22 22:03:42 +1100

    Basic theme based on PureCSS blog theme.

--- config.rb
+++ config.rb
@@ -6,10 +6,10 @@
 
 activate :blog do |blog|
   # blog.prefix = "blog"
-  # blog.permalink = ":year/:month/:day/:title.html"
-  # blog.sources = ":year-:month-:day-:title.html"
+  blog.permalink = ":title.html"
+  blog.sources = ":year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
-  # blog.layout = "layout"
+  blog.layout = "layout_post"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = ":year.html"
@@ -17,17 +17,16 @@ activate :blog do |blog|
   # blog.day_link = ":year/:month/:day.html"
   # blog.default_extension = ".markdown"
 
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  # blog.tag_template = "tag.html"
 
   # blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/:num"
 end
 
-page "/feed.xml", :layout => false
+page "/feed.xml", layout: false
 
-### 
+###
 # Compass
 ###
 
@@ -45,13 +44,13 @@ page "/feed.xml", :layout => false
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -86,21 +85,26 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
-  
+
   # Minify Javascript on build
   # activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
+
+activate :syntax, line_numbers: true
+
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true
